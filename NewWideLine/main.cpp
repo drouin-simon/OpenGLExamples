56a44b3c104d2c04f7d0e66082968a2819074f8e
Simon Drouin <drouin.simon@gmail.com>
2016-02-05 01:48:22 -0500

    NewWideLine working almost. Math needs improvement.

--- NewWideLine/main.cpp
+++ NewWideLine/main.cpp
@@ -14,9 +14,9 @@ const char appName[] = "NewWideLine";
 
 GlslShader * shader = 0;
 Vec2 pt0( 200.0, 200.0 );
-Vec2 pt1( 400.0, 400.0 );
+Vec2 pt1( 300.0, 300.0 );
 float w0 = 50.0;
-float w1 = 100.0;
+float w1 = 200.0;
 float c0 = 0.5;
 float c1 = 1.0;
 
@@ -25,6 +25,7 @@ void main() \
 { \
     gl_Position = ftransform(); \
     gl_TexCoord[0] = gl_MultiTexCoord0; \
+    gl_TexCoord[1] = gl_MultiTexCoord1; \
     gl_FrontColor = gl_Color; \
 } ";
 
@@ -33,18 +34,14 @@ const char * shaderCode = "#version 120\n\
 void main() \
 { \
     float s = gl_TexCoord[0].x; \
-    float t = 2.0 * abs( gl_TexCoord[0].y - 0.5 ); \
-    float r = gl_TexCoord[0].z; \
-    float c = gl_TexCoord[0].w; \
-    float f = s + sqrt( clamp( r*r - t*t, 0.0, 1.0 ) ); \
+    float sp = gl_TexCoord[0].y; \
+    float t = abs( gl_TexCoord[0].z ); \
+    float r = gl_TexCoord[1].x; \
+    float c = gl_TexCoord[1].y; \
+    float f = s + sqrt( max( 0.0, r*r - t*t ) ); \
+    f = f / ( s + sp ); \
     gl_FragColor = gl_Color; \n\
     gl_FragColor.a = f; \n\
-} ";
-
-const char * shaderCodeMin = "#version 120\n\
-void main() \
-{ \
-    gl_FragColor = gl_Color; \n\
 } ";
 
 void display(void)
@@ -70,43 +67,44 @@ void display(void)
     }
 	
     // Compute coords of the quad
-    float halfWidth = 0.5 * std::max( w0, w1 );
+    double w = std::max( w0, w1 );
+    double r = 0.5 * w;
     Vec2 dir = pt1 - pt0;
+    double l = len( dir );
     Vec2 normDir( dir );
     normDir.Normalise();
     Vec2 right( dir[1], -dir[0] );
     right.Normalise();
-    Vec2 bl = pt0 - halfWidth * right - halfWidth * normDir;
-    Vec2 br = pt0 + halfWidth * right - halfWidth * normDir;
-    Vec2 jl = pt1 - halfWidth * right;
-    Vec2 jr = pt1 + halfWidth * right;
-    Vec2 tr = pt1 + halfWidth * right + halfWidth * normDir;
-    Vec2 tl = pt1 - halfWidth * right + halfWidth * normDir;
+    Vec2 bl = pt0 - r * right - r * normDir;
+    Vec2 br = pt0 + r * right - r * normDir;
+    Vec2 jl = pt1 - r * right;
+    Vec2 jr = pt1 + r * right;
+    Vec2 tr = pt1 + r * right + r * normDir;
+    Vec2 tl = pt1 - r * right + r * normDir;
 
     // Setup shader and vars
-    float r0 = 0.5 * w0 / halfWidth;
-    float r1 = 0.5 * w1 / halfWidth;
+    double r0 = 0.5 * w0;
+    double r1 = 0.5 * w1;
+    double rl = l + r0;
     shader->UseProgram( true );
-
-    // TODO : switch r to world instead of 0.0-1.0
 
     // Draw the quad
     glColor4d( 1.0, 1.0, 1.0, 1.0 );
     glBegin( GL_QUADS );
 
-        // body
-        //glColor4d( 1.0, 1.0, 1.0, 1.0 );
-        glTexCoord4d( 0.0, 0.0, r0, c0 ); glVertex( bl );
-        glTexCoord4d( 0.0, 1.0, r0, c0 ); glVertex( br );
-        glTexCoord4d( 1.0, 1.0, r1, c1 ); glVertex( jr );
-        glTexCoord4d( 1.0, 0.0, r1, c1 ); glVertex( jl );
+    // body
+    //glColor4d( 1.0, 1.0, 1.0, 1.0 );
+    glTexCoord4d( 0.0, rl,  -r, 0.0 ); glMultiTexCoord2d(	GL_TEXTURE1, r0, c0 );glVertex( bl );
+    glTexCoord4d( 0.0, rl,   r, 0.0 ); glMultiTexCoord2d(	GL_TEXTURE1, r0, c0 ); glVertex( br );
+    glTexCoord4d( rl,  0.0,  r, 0.0 ); glMultiTexCoord2d(	GL_TEXTURE1, r1, c1 ); glVertex( jr );
+    glTexCoord4d( rl,  0.0, -r, 0.0 ); glMultiTexCoord2d(	GL_TEXTURE1, r1, c1 ); glVertex( jl );
 
-        // front cap
-        //glColor4d( 1.0, 1.0, 0.0, 1.0 );
-        glTexCoord4d( 1.0, 0.0, r1, c1 ); glVertex( jl );
-        glTexCoord4d( 1.0, 1.0, r1, c1 ); glVertex( jr );
-        glTexCoord4d( 1.0, 1.0, r1, c1 ); glVertex( tr );
-        glTexCoord4d( 1.0, 0.0, r1, c1 ); glVertex( tl );
+    // front cap
+    //glColor4d( 1.0, 1.0, 0.0, 1.0 );
+    glTexCoord4d( rl, 0.0, -r, 0.0 ); glMultiTexCoord2d( GL_TEXTURE1, r1, c1 ); glVertex( jl );
+    glTexCoord4d( rl, 0.0,  r, 0.0 ); glMultiTexCoord2d( GL_TEXTURE1, r1, c1 ); glVertex( jr );
+    glTexCoord4d( rl, 0.0,  r, 0.0 ); glMultiTexCoord2d( GL_TEXTURE1, r1, c1 ); glVertex( tr );
+    glTexCoord4d( rl, 0.0, -r, 0.0 ); glMultiTexCoord2d( GL_TEXTURE1, r1, c1 ); glVertex( tl );
 
 	glEnd();
 
